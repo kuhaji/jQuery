0b3cd8f5ba94d8645fcdf23db75c3a696553f92d
Juha <juhavali@metropolia.fi>
2021-09-19 20:42:20 +0300

    Second commit

--- js/jqlab1.js
+++ js/jqlab1.js
@@ -1,3 +1,23 @@
-$("p:second").add('id', 'second')
-$("p:first").remove();
-$("#fdiv").css("background-color","yellow");
+$('p:eq(1)').attr('id', 'sp');
+$('#fp').remove();
+$('#fdiv').css({
+    backgroundColor: 'grey',
+    color: 'white'
+}
+);
+
+$("button").click(function(){
+    $('#sp').after("<p>Hello user</p>");
+});
+
+
+$('#4p').hide();
+
+$('#sdiv').hover(
+    function () {
+        $('#4p').text('About to select a link').show();
+    },
+    function () {
+        $('#4p').hide();
+    }
+)
